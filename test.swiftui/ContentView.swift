7ab60d11f7f0f28f607612a79d49745fdbb08d56
Bogdan Sarkisov <b.sarkisov@mobidev.biz>
2024-02-12 15:01:10 +0200

    Initial Commit

---
+++ test.swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test.swiftui
+//
+//  Created by User on 12.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test.swiftui/test_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_swiftuiApp.swift
+//  test.swiftui
+//
+//  Created by User on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct test_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
